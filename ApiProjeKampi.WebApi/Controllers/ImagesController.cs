ad1583a899454e8f700cc908008a74cd620a42d9
Gülistan Aksoy <gulistanaksoy87@gmail.com>
2025-08-28 12:02:30 +0300

    ImagesController eklendi

---
+++ ApiProjeKampi.WebApi/Controllers/ImagesController.cs
@@ -0,0 +1,64 @@
+﻿using ApiProjeKampi.WebApi.Context;
+using ApiProjeKampi.WebApi.Dtos.ImageDtos;
+using ApiProjeKampi.WebApi.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        readonly ApiContext _context;
+        readonly IMapper _mapper;
+        public ImagesController(ApiContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> ImageList()
+        {
+            var values = await _context.Images.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateImageDto createImageDto)
+        {
+            var value = _mapper.Map<Image>(createImageDto); 
+            await _context.Images.AddAsync(value);
+            await _context.SaveChangesAsync();
+            return (Ok("Resim eklendi."));
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteImage(int id)
+        {
+            var value = await _context.Images.FindAsync(id);
+            if (value == null)
+                return NotFound("Resim bulunamadı");
+            _context.Images.Remove(value);
+            await _context.SaveChangesAsync();
+            return Ok("Resim silindi.");
+        }
+        [HttpGet("GetImage")]
+        public async Task<IActionResult> GetImage(int id)
+        {
+            var value = await _context.Images.FindAsync(id);
+            if (value == null)
+                return NotFound("Resim bulunamadı.");
+            return Ok(value);
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateImage(UpdateImageDto updateImageDto)
+        {
+            var value = _mapper.Map<Image>(updateImageDto);
+            _context.Images.Update(value);
+            await _context.SaveChangesAsync();
+            return Ok("Resim güncellendi.");
+        }
+    }
+}
--- ApiProjeKampi.WebApi/Mapping/GeneralMapping.cs
+++ ApiProjeKampi.WebApi/Mapping/GeneralMapping.cs
@@ -1,6 +1,7 @@
 ﻿using ApiProjeKampi.WebApi.Dtos.AboutDtos;
 using ApiProjeKampi.WebApi.Dtos.CategoryDtos;
 using ApiProjeKampi.WebApi.Dtos.FeatureDtos;
+using ApiProjeKampi.WebApi.Dtos.ImageDtos;
 using ApiProjeKampi.WebApi.Dtos.MessageDtos;
 using ApiProjeKampi.WebApi.Dtos.NotificationDtos;
 using ApiProjeKampi.WebApi.Dtos.ProductDtos;
@@ -46,6 +47,11 @@ namespace ApiProjeKampi.WebApi.Mapping
             CreateMap<Reservation, ResultReservationDto>().ReverseMap();
             CreateMap<Reservation, GetReservationByIdDto>().ReverseMap();
             CreateMap<Reservation, UpdateReservationDto>().ReverseMap();
+
+            CreateMap<Image, CreateImageDto>().ReverseMap();
+            CreateMap<Image, UpdateImageDto>().ReverseMap();
+            CreateMap<Image, ResultImageDto>().ReverseMap();
+            CreateMap<Image, GetImageByIdDto>().ReverseMap();
 
 
         }
